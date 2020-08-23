52185df1b4a46bc2a9c5560bd465288cdf746a5f
s0phia- <sophiajones@live.co.uk>
2020-08-23 16:47:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-# file to run for easy21 coursework
-import config
-import monteCaro
-import sarsa
-import visualisations 
+# file to run for easy21 
+#import config
+from agents.monteCarlo import MonteCarlo
+from agents.sarsa import Sarsa
+#import sarsa
+#import visualisations 
+
+import numpy as np
 
 if __name__ == "__main__":
     # do stuff
+    x = MonteCarlo(no_episodes = 1000000)
+    q = x.learn()
+    t = [(p+1, d+1, np.argmax(q[p, d, :])) for p in range(0,21) for d in range(0,10)]       
