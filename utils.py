acf1ae39150a110930421adc3e2f7a72b0ac69fb
So <sophiajones@live.co.uk>
2020-09-09 23:57:46 +0200

    add mse

--- utils.py
+++ utils.py
@@ -17,9 +17,24 @@ def ep_greedy(Nsa, Qsa, state, N_0, actions = actions):
     return(action)
     
 # version of epsilon greedy to work with function approximators
-def fa_ep_greedy(state, weights, phi_ftn, epsilon, actions = actions) :
-    if np.random.rand() < (1 - epsilon):
-        action = np.argmax(phi_ftn(state, a)*weights for a in actions)
+def fa_ep_greedy(state, weights, phi_ftn, epsilon, actions = actions):
+    if random.random() > epsilon:
+        action_values = [np.dot(phi_ftn(state, a),weights) for a in actions]
+        action = np.argmax(action_values)
     else:
         action = np.random.choice(actions)
     return(action)  
+
+def expand_coarse_code(coarse_code, p_range, d_range, a_range, encoding, 
+                       state_space_shape, weights):
+    Q = np.zeros(state_space_shape)
+    for p in p_range:
+        for d in d_range:
+            for a in a_range:
+                #print(a, p , d, sum(encoding((p, d), a)))
+                Q[p-1, d - 1, a] += np.dot(encoding((p, d), a),weights)
+    return(Q)
+    
+def mse(X,Y):
+    mse = np.sum((X-Y)**2)/np.size(X)
+    return(mse)
